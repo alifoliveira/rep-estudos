7ff2b7cb5e869b6d44784f3b0d443a0a5bbae16e
Álif Oliveira <53924906+alifoliveira@users.noreply.github.com>
2020-07-08 20:10:07 -0500

    final da aula

---
+++ python/Alura/data science/numpy/operacoes_matematicas.py
@@ -0,0 +1,23 @@
+adição = 2 + 2
+print(adição)
+
+subtração = 2 - 2
+print(subtração)
+
+multiplicação = 2 * 3
+print(multiplicação)
+
+divisão = 10 / 3
+print(divisão)
+
+divisão_inteira = 10 // 3
+print(divisão_inteira)
+
+exponenciação = 2 ** 3
+print(exponenciação)
+
+resto_divisão = 10 % 3
+print(resto_divisão)
+
+expressao1 = (5 * 2) + (3 * 2)
+print(expressao1)
