817b07c870fc2aa6505e5543bfbcd3a7525464f2
Álif Oliveira <53924906+alifoliveira@users.noreply.github.com>
2020-07-10 19:13:45 -0500

    salvamento

--- ruby/Alura/Ruby I/maior_menor.rb
+++ ruby/Alura/Ruby I/maior_menor.rb
@@ -3,25 +3,23 @@
 # == : igual
 # .to_i : converte para inteiro
 
-nova_linha = "\n"
+puts "Bem vindo ao jogo da adivinhação"
 
-puts "Bem vindo ao jogo da adivinhação" + nova_linha
-
-puts "Qual é o seu nome?" + nova_linha
+puts "Qual é o seu nome?"
 nome = gets
-puts
+puts "\n\n\n"
 
-puts "Começaremos o jogo para você, " + nome + nova_linha
+puts "Começaremos o jogo para você, " + nome
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts
 
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?" + nova_linha
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
 puts
 
-puts "Será que acertou? Você chutou " + chute + nova_linha
+puts "Será que acertou? Você chutou " + chute
 puts chute.to_i == numero_secreto # converte para inteiro e compara
